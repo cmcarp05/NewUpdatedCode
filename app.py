09a654d235749e0a2c5b9d369981170abec510e6
cmcarp05 <146238066+cmcarp05@users.noreply.github.com>
2023-12-02 03:59:21 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -4,9 +4,26 @@ import pandas as pd
 import os
 import shutil
 from kaggle.api.kaggle_api_extended import KaggleApi
-
+from io import BytesIO
+import base64
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
+
+from flask import render_template
+from flask_mail import Mail, Message
+
+mail = Mail(app)
+
+@app.errorhandler(500)
+def internal_error(error):
+    # Send an email to your support email with the error details
+    msg = Message('Internal Server Error', sender='cmcarp05@louisville.edu', recipients=['ccarpenter0527@gmail.com'])
+    msg.body = str(error)
+    mail.send(msg)
+    return render_template('500.html'), 500
 
 from flask import Response
 
@@ -119,18 +136,70 @@ def get_csv_from_kaggle():
         print(f"File {filepath} not found. Check if the dataset contains 'database.csv'.")
         return None
 
+from flask import request
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    # Get the search query and filter criteria from the form
+    search_query = request.form.get('search_query')
+    filter_fuel_type1 = request.form.get('filter_fuel_type1')
+    filter_fuel_type2 = request.form.get('filter_fuel_type2')
+
+    # Get the data
+    fuel_data = get_csv_from_kaggle()
+
+    # If the data is successfully retrieved, convert it to records for display
+    if fuel_data is not None:
+        if search_query:
+            # Filter data based on the search query (modify this logic as needed)
+            fuel_data = fuel_data[fuel_data['Make'].str.contains(search_query, case=False) |
+                                  fuel_data['Model'].str.contains(search_query, case=False)]
+        
+        if filter_fuel_type1:
+            # Filter data by Fuel Type 1
+            fuel_data = fuel_data[fuel_data['Fuel Type 1'] == filter_fuel_type1]
+
+        if filter_fuel_type2:
+            # Filter data by Fuel Type 2
+            fuel_data = fuel_data[fuel_data['Fuel Type 2'] == filter_fuel_type2]
+
+        data_records = fuel_data.to_dict(orient='records')
+        return render_template('index.html', data=data_records, search_query=search_query)
+    else:
+        return render_template('error.html', message="Data could not be loaded.")
+
+@app.route('/page1')
+def page1():
     # Get the data
     fuel_data = get_csv_from_kaggle()
 
     # If the data is successfully retrieved, convert it to records for display
     if fuel_data is not None:
         data_records = fuel_data.to_dict(orient='records')
-        return render_template('/index.html', data=data_records)
+
+        # Create the chart
+        plt.figure(figsize=(8, 4))
+        sorted_data = fuel_data.groupby('Fuel Type 1')['Annual Fuel Cost (FT1)'].mean().sort_values(ascending=False)
+        sorted_data.plot(kind='bar', color='skyblue')
+        plt.title('Average Annual Fuel Cost (FT1) by Fuel Type 1')
+        plt.xlabel('Fuel Type 1')
+        plt.ylabel('Average Annual Fuel Cost (FT1)')
+        plt.xticks(rotation=45, ha='right')  # Rotate x-axis labels for better readability
+        plt.tight_layout()
+
+        # Save the plot to a BytesIO object
+        img = BytesIO()
+        plt.savefig(img, format='png')
+        img.seek(0)
+
+        # Convert the image to base64 for embedding in HTML
+        img_str = base64.b64encode(img.read()).decode('utf-8')
+
+        # Pass the data and image to the template
+        return render_template('page1.html', data_records=data_records, img_str=img_str)
     else:
         return render_template('error.html', message="Data could not be loaded.")
+
 
 
 if __name__ == '__main__':
@@ -149,4 +218,3 @@ if __name__ == '__main__':
         app.run(debug=True)
     if not debug:
         app.run()
-
